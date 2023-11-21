8f04040e15f4adcb3515944775d51a5a50db8aa8
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-21 11:45:49 +0300

    feat: complete loops master-task

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -4,6 +4,17 @@ public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
 
+        for (int i = 0; i <= cathetusLength - 1; i++) {
+            for (int j = 1; j <= cathetusLength; j++){
+                if (j >= cathetusLength - i) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
+
     }
 
     public static void main(String[] args) {
