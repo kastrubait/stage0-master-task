5f850e522b8eb88405f00b1361bd42bf32cd0b97
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-21 11:52:58 +0300

    feat: complete conditionss master-task

--- src/main/java/com/epam/conditions/IntegerDivider.java
+++ src/main/java/com/epam/conditions/IntegerDivider.java
@@ -3,6 +3,15 @@ package com.epam.conditions;
 public class IntegerDivider {
 
     public void printCompletelyDivided(int dividend, int divider) {
+        int res;
+        if (divider != 0) {
+            res = dividend / divider;
+            System.out.println(res * divider == dividend
+                    ? "can be divided completely"
+                    : "cannot be divided completely");
+        } else {
+            System.out.println("division by zero");
+        }
     }
 
 }
