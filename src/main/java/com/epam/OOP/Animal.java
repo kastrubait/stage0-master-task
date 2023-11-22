121f6b42566a8939dec0619795d2ad5fc0d7536b
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-22 15:39:23 +0300

    feat: complete oop master-task

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -2,4 +2,17 @@ package com.epam.OOP;
 
 public class Animal {
 
+    String color;
+    int numberOfPaws;
+    boolean hasFur;
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+    public String getDescription() {
+        String ans = hasFur ? "a" : "no";
+        String valuePaw = (numberOfPaws == 1) ? " paw" : " paws";
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + valuePaw + " and " + ans + " fur.";
+    }
 }
--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -1,5 +1,15 @@
 package com.epam.OOP;
 
-public class Bird {
+public class Bird extends Animal {
+    public Bird(){
+        super("blue", 2,  false);
+    }
+
+    public String getDescription() {
+        String ans = hasFur ? "a" : "no";
+        String valuePaw = (numberOfPaws == 1) ? " paw" : " paws";
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + valuePaw + " and " + ans + " fur. Moreover, it has 2 wings and can fly.";
+    }
 
 }
+
